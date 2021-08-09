9a78f73751b0161ac29a07ee06bf6d31e02cbb58
kosaka123 <messi_ong@hotmail.com>
2021-08-09 17:25:52 +0800

    day-2-build

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ exploreData, cardsData }) {
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {exploreData?.map((item) => (
               <SmallCard
-                key={item.id}
+                key={item.img}
                 img={item.img}
                 distance={item.distance}
                 location={item.location}
